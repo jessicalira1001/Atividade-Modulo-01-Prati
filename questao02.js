c54a317e5ecb6c50fa41446f348062a2ed91cf60
jessicalira1001 <jessicalira1001@gmail.com>
2024-04-30 17:51:42 -0300

    Resolucao da questão 02

---
+++ questao02.js
@@ -0,0 +1,8 @@
+let numeroEleitores = 200;
+let votosBrancos = 10;
+let votosNulos = 20;
+let votosValidos = 160;
+
+console.log(((votosBrancos/numeroEleitores) * 100) + " de Votos brancos")
+console.log(((votosNulos/numeroEleitores) * 100) + " de Votos Nulos")
+console.log(((votosValidos/numeroEleitores) * 100) + " de Votos Válidos")
