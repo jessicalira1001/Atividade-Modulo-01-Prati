eaf96b66b65191c530d223ebdafc596b2a0712e5
jessicalira1001 <jessicalira1001@gmail.com>
2024-04-30 18:01:50 -0300

    Resolucao da questão 03

---
+++ questao03.js
@@ -0,0 +1,14 @@
+const a = 100;
+let b = 100;
+let c = 20;
+let d = a + b + c;
+
+
+let aNovo = a + 25;
+let bNovo = b * 3;
+let cNovo = ((c/100)*12)
+
+console.log(aNovo)
+console.log(bNovo)
+console.log(cNovo)
+console.log(d)
