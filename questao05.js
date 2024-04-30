28b05699fd831937a4153f2480d54a9dcf527a5a
jessicalira1001 <jessicalira1001@gmail.com>
2024-04-30 18:09:08 -0300

    Resolucao da questão 05

---
+++ questao05.js
@@ -0,0 +1,8 @@
+let notaAvaliacao1 = 7;
+let notaAvaliacao2 = 4;
+
+if(((notaAvaliacao1 + notaAvaliacao2)/2) >= 6){
+    console.log("PARABÉNS! Você foi aprovado")
+}else{
+    console.log("Você foi REPROVADO! Estude mais")
+}
